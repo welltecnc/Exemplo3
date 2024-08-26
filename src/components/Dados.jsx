4dccd38350cf78ef1069cd354c0321541d3dda86
Wellington <welltecnic@hotmail.com>
2024-08-26 09:39:36 -0300

    Criando Dados com desestruct

---
+++ src/components/Dados.jsx
@@ -0,0 +1,6 @@
+/*CRIANDO UM OBJETO DADOS E EXPORTANDO */
+export const Dados ={
+    nome:"Fiap",
+    email:"fiap@fiap.com",
+    idade:30
+}
--- src/routes/Aluno.jsx
+++ src/routes/Aluno.jsx
@@ -1,11 +1,16 @@
-const Aluno=()=>{
+import {Dados} from '../components/Dados'
 
+const Aluno=()=>{
+    //trazendo os dados com desestrcut
+    
+    const {nome,email,idade}=Dados
+    
     return(
         <>
         <h1>PÁGINA ALUNO</h1>
-        <p>Nome do Aluno:</p>
-        <p>Email do Aluno:</p>
-        <p>Idade do Aluno:</p>
+        <p>Nome do Aluno:{nome}</p>
+        <p>Email do Aluno:{email}</p>
+        <p>Idade do Aluno:{idade}</p>
         </>
     )
 }
